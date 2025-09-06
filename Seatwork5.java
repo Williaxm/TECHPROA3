89d6981278b81a5e9e9b6decf81f4fb5ac6c329a
Williaxm <docenawilliam99@gmail.com>
2025-09-06 19:29:50 +0800

    Update Seatwork5.java

--- Seatwork5.java
+++ Seatwork5.java
@@ -1,1 +1,15 @@
+public class Seatwork5 {
+    public static void main(String[] args) {
+    
+        System.out.println("SEATWORKFIVE");
 
+ int countdown = 3;
+
+ while (countdown > 0) {
+    
+    System.out.println(countdown);
+    countdown--;
+ }
+    System.out.println("Happy New Year!!");
+    }
+}
