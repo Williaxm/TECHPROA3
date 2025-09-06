fb9826bfd5a5d81706a83668c0d4cb7e85feb48c
Williaxm <docenawilliam99@gmail.com>
2025-09-06 19:10:45 +0800

    Create Seatwork7.java

---
+++ Seatwork7.java
@@ -0,0 +1,22 @@
+public class SeatworkSeven {
+public static void main (String args []) {
+
+switch(grade) {
+case `A`:
+System.out.println ("Excellent");
+break;
+case `B`:
+case `C`:
+System.out.println("Well done");
+break;
+case `D`:
+System.out.println("You passed");
+  case `F`;
+    System.out.println("Better Try again");
+    break;
+    default;
+      System.out.println("Invalid grade");
+}
+  System.out.println("Your grade is" + grade);
+}
+}
