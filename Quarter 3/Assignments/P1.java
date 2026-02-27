6270389a56ff654a97fec33c2e7fcef608e6c8a8
Williaxm <docenawilliam99@gmail.com>
2026-02-27 14:59:39 +0800

    Update and rename 1 to P1.java

---
+++ Quarter 3/Assignments/P1.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+public class Example1 {
+    public static void main(String[] args) {
+        
+        JFrame frame = new JFrame("Test1");
+        frame.setSize(400, 300);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+}
+//Output on VScode opened a blank new window on the task bar named "Test1"
